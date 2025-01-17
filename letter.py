176a208941aed8b0ba9703d046d79ee1b00acf40
ramuallam7731 <ramuallam77@gmail.com>
2025-01-17 17:24:55 +0530

    first commit

---
+++ letter.py
@@ -0,0 +1,9 @@
+c=input() 
+if c.isupper():
+    print("Uppercase Letter") 
+elif c.islower():
+    print("Lowercase Letter") 
+elif c.isdigit():
+    print("Digit") 
+else:
+    print("Special Character")
